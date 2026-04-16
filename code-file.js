7de98aaca744cd3186cde771ce19febb5b4f5a11
Kaan Özkilic <kaan.ozkilic@student.hamk.fi>
2026-04-16 10:05:52 +0000

    AB#52 Login registration using social accounts

--- code-file.js
+++ code-file.js
@@ -6,3 +6,4 @@ const images = ["img1", "img2", "img3"];
 const finalPrice = 999.99;
 const isGuestCheckout = true;
 const abandonedCartRecovery = true;
+const loginRegistrationUsingSocialAccounts = true;
