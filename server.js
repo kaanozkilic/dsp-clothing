3c2c6db471936bf9ffc9ff06bab5e27407d3cc81
Shuvojyoti Singha <sjshuvosingha@gmail.com>
2026-05-05 19:00:27 +0300

    server.js fixed

--- server.js
+++ server.js
@@ -3,26 +3,17 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-const teamMembers = [
-  "Shuvo"
-];
-
 app.get("/", (req, res) => {
-  res.send(`
+  res.send(
     <html>
       <body style="font-family: Arial; padding: 40px;">
         <h1>DSP Clothing Store</h1>
-        <p>End-to-End CI/CD Pipeline Demo</p>
 
         <h2>Team Members</h2>
         <ul>
-          ${teamMembers.map(name => `<li>${name}</li>`).join("")}
+          <li>Shuvo</li>
         </ul>
       </body>
-    </html>
-  `);
-});
-
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}`);
+    </html>,
+  );
 });
